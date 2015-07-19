d9a55e5a1635c48c8fbbcfc2a33497fb5a8b38ed
Jose Antonio Cordero <jcordero93@alumno.uned.es>
2015-07-19 16:33:16 +0200

    Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+//Importamos el controlador de la pregunta
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+
+//Registrar las nuevas rutas en el enrutador.
+/*GET /quizes/question */
+router.get('/quizes/question', quizController.question);
+/*GET /quizes/answer */
+router.get('/quizes/answer'  , quizController.answer);
+
 
 module.exports = router;
