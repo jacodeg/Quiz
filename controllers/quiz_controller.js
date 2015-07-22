12a008c7bb1657b7c5784b28ffc9a6c687bf48fc
Jose Antonio Cordero <jcordero93@alumno.uned.es>
2015-07-22 04:19:42 +0200

    Ejercicio P2P-Módulo 7
    	modificado: controllers/quiz_controller.js
    	modificado: routes/index.js
    	modificado: views/quizes/index.ejs

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,12 +14,44 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
-exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+//GET /quizes?search=texto_a_buscar
+exports.index = function(req, res){
+
+  if(!req.query.search){    // GET /quizes
+    var condicion = {order : "pregunta ASC"};
+    models.Quiz.findAll(condicion).then(function(quizes){
+      res.render('quizes/index', {quizes: quizes});
+    }).catch(function(error) { next(error);})
+
+  }else{ //GET /quizes?search=texto_a_buscar
+    //Delimitamos el contenido antes y despues con %
+    var search = '%' + req.query.search + '%';
+    // Cambiamos los espacios por %
+    search = search.replace(' ', '%');
+
+    // Le pasamos a findAll un objeto con los parámetros de la búsqueda
+    //Si finAll termina correctamente el metodo la promesa ejecuta el callback
+    //que renderiza el resultado del a búsqueda, es decir, renderiza el
+    //el array devuelto. En caso de ocurrir un error, se captura con .catch()
+    // y ejecuta el callback encargado de gestionar el error.
+    //http://docs.sequelizejs.com/en/latest/docs/querying/#operators
+    //NOTA en las ref. dice que el operador se escribe con $like, sin embargo
+    //me dar un error "Possibly unhandled ReferenceError: next is not defined"
+    //cuando intenta construir la consulta sql. Sin $, funciona correctamente
+    //Y me construye la URL
+    var condicion= { where : { pregunta: {
+                      like: search
+                      }} ,
+                  order : 'pregunta'};
+
+    //var condicion = { where: ['pregunta LIKE ?', search],
+                // order : 'pregunta ASC'};
+    models.Quiz.findAll(condicion)
+        .then(function(quizes){
+            res.render('quizes/index', {quizes: quizes});
+          }).catch(function(error) { next(error);})
+  }
+
 };
 
 // GET /quizes/:id
