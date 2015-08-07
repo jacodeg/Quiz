3e96fb73930dfe03ae7415af0d0417799e5babe3
Jose Antonio Cordero <jcordero93@alumno.uned.es>
2015-08-07 04:46:12 +0200

    Moderación de usuarios

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
-// Autoload de comandos con :quizId
+// Autoload de comandos con :quizId y con :commentId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login',  sessionController.new);     // formulario login
@@ -36,6 +37,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',  sessionController.loginRequired, commentController.publish);
 
 //Registrar la ruta de créditos
 /*GET author*/
